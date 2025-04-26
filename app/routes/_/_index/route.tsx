f86d882f2610a96cd21cb851ab9157a7704d9574
rossipedia <bryan.ross@docker.com>
2025-04-26 17:11:30 -0600

    expand dark variant to fix specificity issue when using color-scheme

--- app/routes/_/_index/route.tsx
+++ app/routes/_/_index/route.tsx
@@ -39,6 +39,7 @@ export default function Index() {
           onClick={() => {
             revalidate();
           }}
+          variant="outline"
         >
           Revalidate
         </Button>
